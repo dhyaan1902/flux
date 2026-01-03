ec7e3772171b0ba201fd8ea2f4c1dbf05fc80035
dhyaan19022009-hue <dhyaan19022009@gmail.com>
2026-01-03 15:27:28 +0530

    video player optimasaiton

--- services/stream.ts
+++ services/stream.ts
@@ -1,23 +1,29 @@
+import { MediaType } from '../types';
 
-import { MediaType } from "../types";
+export const getEmbedUrl = (
+  server: string,
+  tmdbId: string | number,
+  imdbId: string | undefined,
+  type: MediaType,
+  season?: number,
+  episode?: number,
+) => {
+  const isMovie = type === MediaType.MOVIE;
+  // Prefer IMDb ID if available, otherwise fallback to TMDb ID
+  const id = imdbId || tmdbId;
 
-export const getEmbedUrl = (server: string, tmdbId: string | number, imdbId: string | undefined, type: MediaType, season?: number, episode?: number) => {
-    const isMovie = type === MediaType.MOVIE;
-    // Prefer IMDb ID if available, otherwise fallback to TMDb ID
-    const id = imdbId || tmdbId;
+  switch (server) {
+    case 'vidrock':
+      // VidRock logic
+      return isMovie
+        ? `https://vidrock.net/movie/${id}?autoplay=true&theme=4470ad`
+        : `https://vidrock.net/tv/${id}/${season}/${episode}?autoplay=true&theme=4470ad`;
 
-    switch (server) {
-        case 'vidrock':
-            // VidRock logic
-            return isMovie
-                ? `https://vidrock.net/movie/${id}`
-                : `https://vidrock.net/tv/${id}/${season}/${episode}`;
-        
-        case 'vidsrc':
-        default:
-             // VidSrc logic
-             return isMovie
-                ? `https://vidsrc.cc/v2/embed/movie/${id}?autoPlay=false`
-                : `https://vidsrc.cc/v2/embed/tv/${id}/${season}/${episode}?autoPlay=false`;
-    }
+    case 'vidsrc':
+    default:
+      // VidSrc logic
+      return isMovie
+        ? `https://vidsrc.cc/v2/embed/movie/${id}?autoPlay=false`
+        : `https://vidsrc.cc/v2/embed/tv/${id}/${season}/${episode}?autoPlay=false`;
+  }
 };
